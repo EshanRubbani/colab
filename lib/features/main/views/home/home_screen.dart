8e499fb85fde55892abf72cd91acb586f0a2d14f
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-05-11 22:59:04 +0500

    Responsiveness Done

--- lib/features/main/views/home/home_screen.dart
+++ lib/features/main/views/home/home_screen.dart
@@ -226,162 +226,157 @@ _buildForDesktop(Size size) {
         child: ListView.builder(
             itemCount: 10,
             itemBuilder: (context, index) {
-              return Expanded(
-                child: Container(
-                  constraints: BoxConstraints.expand(
-                    height: size.height * 0.8,
-                  ),
-                  margin: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.grey.withOpacity(0.1),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 0.1,
-                        offset:
-                            const Offset(0, 2), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: size.height * 0.4,
-                        decoration: const BoxDecoration(
-                          color: Colors.grey,
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/home/piano.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Stack(
-                          alignment: Alignment.bottomRight,
-                          children: [
-                            Positioned(
-                              right: 70,
-                              bottom: 10,
-                              child: Container(
-                                height: 40,
-                                width: 40,
-                                decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                ),
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Icons.ios_share),
-                                  color: KAppColors.kPrimary,
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              right: 20,
-                              bottom: 10,
-                              child: Container(
-                                height: 40,
-                                width: 40,
-                                decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                ),
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                      Icons.favorite_border_outlined),
-                                  color: KAppColors.kPrimary,
-                                ),
-                              ),
-                            ),
-                          ],
+              return Container(
+                constraints: BoxConstraints(maxHeight: 500),
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey.withOpacity(0.1),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 0.1,
+                      offset: const Offset(0, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: size.height * 0.4,
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/home/piano.jpg'),
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Row(
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
                         children: [
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            margin: const EdgeInsets.only(left: 10),
-                            decoration: BoxDecoration(
-                              color: KAppColors.kPrimary,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/icons/google.png',
-                                width: 20,
-                                height: 20,
-                                fit: BoxFit.fill,
+                          Positioned(
+                            right: 70,
+                            bottom: 10,
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.ios_share),
+                                color: KAppColors.kPrimary,
                               ),
                             ),
                           ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          const Text(
-                            'Home',
-                            style: TextStyle(fontSize: 16),
+                          Positioned(
+                            right: 20,
+                            bottom: 10,
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: IconButton(
+                                onPressed: () {},
+                                icon:
+                                    const Icon(Icons.favorite_border_outlined),
+                                color: KAppColors.kPrimary,
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            'Vintage Piano',
-                            style: TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.w800),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          margin: const EdgeInsets.only(left: 10),
+                          decoration: BoxDecoration(
+                            color: KAppColors.kPrimary,
+                            borderRadius: BorderRadius.circular(30),
                           ),
-                          const SizedBox(
-                            height: 30,
+                          child: Center(
+                            child: Image.asset(
+                              'assets/icons/google.png',
+                              width: 20,
+                              height: 20,
+                              fit: BoxFit.fill,
+                            ),
                           ),
-                          Container(
-                            height: 3.48,
-                            width: 350,
-                            child: const Center(
-                              child: LinearProgressIndicator(
-                                color: Colors.deepPurple,
-                                value: 0.7,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          'Home',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          'Vintage Piano',
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w800),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Container(
+                          height: 3.48,
+                          width: 350,
+                          child: const Center(
+                            child: LinearProgressIndicator(
+                              color: Colors.deepPurple,
+                              value: 0.7,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 21,
+                          width: 350,
+                          child: const Row(
+                            children: [
+                              Icon(CupertinoIcons.gift),
+                              SizedBox(
+                                width: 5,
+                                height: 5,
                               ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                            height: 21,
-                            width: 350,
-                            child: const Row(
-                              children: [
-                                Icon(CupertinoIcons.gift),
-                                SizedBox(
-                                  width: 5,
-                                  height: 5,
+                              Text("150\$ Backed"),
+                              Expanded(
+                                child: Text(
+                                  "70%",
+                                  textAlign: TextAlign.end,
                                 ),
-                                Text("150\$ Backed"),
-                                Expanded(
-                                  child: Text(
-                                    "70%",
-                                    textAlign: TextAlign.end,
-                                  ),
-                                )
-                              ],
-                            ),
+                              )
+                            ],
                           ),
-                        ],
-                      )
-                    ],
-                  ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               );
             }),
