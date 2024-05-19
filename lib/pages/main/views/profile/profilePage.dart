65809557c6757ad6a37dffeaed2becd046d8fae3
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-05-20 00:37:11 +0500

    Profile Picture

--- lib/pages/main/views/profile/profilePage.dart
+++ lib/pages/main/views/profile/profilePage.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collab/extras/common/common_button.dart';
 import 'package:collab/extras/utils/Helper/homeitems.dart';
+import 'package:collab/extras/utils/constant/colors.dart';
+import 'package:collab/pages/main/views/home/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -31,7 +36,14 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Future<void> _uploadImage() async {
     if (_image == null) return;
-
+    // show loading circle
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
     try {
       final ref = _storage.ref().child(
           'users/${FirebaseAuth.instance.currentUser!.email.toString()}.jpg');
@@ -48,7 +60,12 @@ class _ProfilePageState extends State<ProfilePage> {
           'userIMG': url,
         });
         print("Firestore doone");
+        navigator!.pop(context);
+        AlertDialog(
+          content: Text("Image Uploaded Successfully"),
+        );
       } catch (e) {
+        navigator!.pop(context);
         AlertDialog(
           content: Text(e.toString()),
         );
@@ -58,15 +75,28 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
+  Future<void> skip() async {
+    return Get.to(HomeScreen());
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text('Image Picker and Upload')),
+      appBar: AppBar(title: Text('Add Profile Image')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _image == null ? Text('No image selected.') : Image.file(_image!),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                constraints: BoxConstraints(maxHeight: 400),
+                child: _image == null
+                    ? Text('No image selected.')
+                    : Image.file(_image!),
+              ),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _pickImage,
@@ -77,6 +107,21 @@ class _ProfilePageState extends State<ProfilePage> {
               onPressed: _uploadImage,
               child: Text('Upload Image'),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: KAppColors.kButtonPrimary,
+                minimumSize: Size(size.width / 2, 55),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              onPressed: skip,
+              child: Text(
+                "Skip",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
           ],
         ),
       ),
