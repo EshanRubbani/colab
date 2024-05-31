ba20f9a62de951bed863cb0939d6a5427ec35cc9
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-06-01 03:08:20 +0500

    profile image duplication fix

--- lib/pages/chatpage/chatpage.dart
+++ lib/pages/chatpage/chatpage.dart
@@ -7,46 +7,32 @@ import 'package:collab/extras/utils/constant/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
 class Chatpage extends StatefulWidget {
   final String receiverEmail;
   final String receiverID;
- Chatpage({ super.key, required this.receiverEmail, required this.receiverID });
+
+  Chatpage({super.key, required this.receiverEmail, required this.receiverID});
 
   @override
   State<Chatpage> createState() => _ChatpageState();
 }
 
 class _ChatpageState extends State<Chatpage> {
-  //text controller
   final TextEditingController _messageController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
-  //chat and auth servoces
   final ChatService _chatService = ChatService();
   final FirestoreService _firestore = FirestoreService();
-
-
-  //focus node for textfieldfocus
   FocusNode myfocusnode = FocusNode();
 
   @override
   void initState() {
     super.initState();
-    //add listner to focus node
-    myfocusnode.addListener((){
-      if(myfocusnode.hasFocus){
-        //cause a delay in opening keyboard and scrool down
-        Future.delayed(Duration(milliseconds: 500),
-        
-        () => scrolldown() );
+    myfocusnode.addListener(() {
+      if (myfocusnode.hasFocus) {
+        Future.delayed(Duration(milliseconds: 500), () => scrolldown());
       }
     });
-
-
-    Future.delayed(const  Duration(milliseconds: 500),
-    () => scrolldown(),
-    );
+    Future.delayed(const Duration(milliseconds: 500), () => scrolldown());
   }
 
   @override
@@ -58,187 +44,145 @@ class _ChatpageState extends State<Chatpage> {
 
   void scrolldown() {
     _scrollController.animateTo(
-      _scrollController.position.maxScrollExtent, 
-      duration: const Duration(seconds: 1), 
-      curve: Curves.fastOutSlowIn
-      );
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(seconds: 1),
+      curve: Curves.fastOutSlowIn,
+    );
   }
 
-  //send message
   void sendMessage() async {
-    if(_messageController.text.isNotEmpty){
+    if (_messageController.text.isNotEmpty) {
       await _chatService.sendMessage(
-        widget.receiverID, 
+        widget.receiverID,
         _messageController.text,
-        );
+      );
     }
-    //after sending clear controller
     _messageController.clear();
     scrolldown();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.grey,
-        centerTitle: true
-        ,
-        title: Text(widget.receiverEmail,style: TextStyle(color: Colors.black),),
-    ),
-    body: Column(
-      children: [
-        //display all messages in chat room
-        Expanded(child: _buildMessageList(),),
-
-        // user input
-        _buildUserInput()
-
-      ],
-    ),
+        centerTitle: true,
+        title: Text(widget.receiverEmail, style: TextStyle(color: Colors.black)),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: _buildMessageList()),
+          _buildUserInput(),
+        ],
+      ),
     );
   }
 
-    //build message list
-  Widget _buildMessageList(){
-
+  Widget _buildMessageList() {
     String senderId = FirebaseAuth.instance.currentUser!.uid;
     return StreamBuilder(
       stream: _chatService.getMessages(senderId, widget.receiverID),
-      builder: (context, snapshot){
-        //errors
-        if(snapshot.hasError){
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
           return const Text("Error");
         }
-
-        //loading
-        if(snapshot.connectionState == ConnectionState.waiting){
+        if (snapshot.connectionState == ConnectionState.waiting) {
           scrolldown();
           return const CircularProgressIndicator(
-            
             color: KAppColors.kPrimary,
           );
         }
-
-
-        //return List Veiw Builder
-
-        return ListView(
-            
-            controller: _scrollController,
-            children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        return ListView.builder(
+          controller: _scrollController,
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            return _buildMessageItem(
+              snapshot.data!.docs[index],
+              index > 0 ? snapshot.data!.docs[index - 1] : null,
+            );
+          },
         );
-
-
-
-  }
+      },
     );
   }
 
-  //build Message item
-  Widget _buildMessageItem(DocumentSnapshot doc){
+  Widget _buildMessageItem(DocumentSnapshot doc, DocumentSnapshot? previousDoc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    Map<String, dynamic>? prevData = previousDoc?.data() as Map<String, dynamic>?;
 
-    //message alignment
-    //is current user?
     bool isCurrentUser = data["senderID"] == _firestore.user!.uid;
+    bool showAvatar = previousDoc == null || prevData?["senderID"] != data["senderID"];
+    var alignment = isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
 
-    
-
-    //if message from current user set it to right
-
-    //if message is from other user set it to left
-    
-    var alignment = isCurrentUser ? Alignment.centerRight :Alignment.centerLeft;
-
-    
     return Container(
       alignment: alignment,
       child: Column(
-        crossAxisAlignment: isCurrentUser ?  CrossAxisAlignment.end : CrossAxisAlignment.start,
+        crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          FutureBuilder<String?>(
-        future: isCurrentUser 
-            ? _firestore.getUserProfileImage(_firestore.user!.email.toString()) 
-            : _firestore.getUserProfileImage(data["senderEmail"]),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator(); // or any placeholder widget
-          } else if (snapshot.hasError) {
-            return Icon(Icons.error); // or any error widget
-          } else {
-            return Profileavator(
-              profileUrl: snapshot.data ?? '',
-            );
-          }
-        },
-      ),
+          if (showAvatar)
+            FutureBuilder<String?>(
+              future: isCurrentUser
+                  ? _firestore.getUserProfileImage(_firestore.user!.email.toString())
+                  : _firestore.getUserProfileImage(data["senderEmail"]),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Icon(Icons.error);
+                } else {
+                  return Profileavator(
+                    profileUrl: snapshot.data ?? '',
+                  );
+                }
+              },
+            ),
           ChatBubble(
             isCurrentUser: isCurrentUser,
             message: data["message"],
-            timestamp: data["timestamp"]
-          ), 
-         
-
+            timestamp: data["timestamp"],
+          ),
         ],
       ),
-      );
+    );
   }
 
-  //build user Input
-  Widget _buildUserInput(){
+  Widget _buildUserInput() {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 20,left: 20,right: 15,top: 20),
+      padding: const EdgeInsets.only(bottom: 20, left: 20, right: 15, top: 20),
       child: Row(
         children: [
-          // text field should get most of the space
-          IconButton(onPressed: (){}, icon: Icon(Icons.attach_file,color: KAppColors.kPrimary,size: 28,)),
-          const SizedBox(width: 5,),
+          IconButton(onPressed: () {}, icon: Icon(Icons.attach_file, color: KAppColors.kPrimary, size: 28)),
+          const SizedBox(width: 5),
           Expanded(
             child: Container(
-              
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(14),
-                color: Colors.grey[300]
-
+                color: Colors.grey[300],
               ),
               child: TextField(
-              focusNode: myfocusnode,
-              onEditingComplete: () {
-                sendMessage();
-              },
+                focusNode: myfocusnode,
+                onEditingComplete: () {
+                  sendMessage();
+                },
                 controller: _messageController,
                 textAlign: TextAlign.start,
-                decoration:  InputDecoration(
-
+                decoration: InputDecoration(
                   hintText: "Write Your Message",
-                  suffixIcon: IconButton(onPressed: sendMessage, icon: const Icon(Icons.send,color: KAppColors.kPrimary,)),
-                 
-
+                  suffixIcon: IconButton(onPressed: sendMessage, icon: const Icon(Icons.send, color: KAppColors.kPrimary)),
                   hintFadeDuration: const Duration(seconds: 1),
-                  
                   border: InputBorder.none,
-                  contentPadding: const EdgeInsets.only(left: 15,top: 12),  
+                  contentPadding: const EdgeInsets.only(left: 15, top: 12),
                   hintStyle: const TextStyle(
-                     color: Color.fromARGB(147, 158, 158, 158),
-                  )
-                
+                    color: Color.fromARGB(147, 158, 158, 158),
+                  ),
                 ),
                 obscureText: false,
-                
               ),
-            )
+            ),
           ),
-      
-      
-          // camera button
-          IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.camera, color: KAppColors.kPrimary,size: 28,)),
-          
-
-          // camera button
-          IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.mic, color: KAppColors.kPrimary,size: 28,)),
-
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.camera, color: KAppColors.kPrimary, size: 28)),
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.mic, color: KAppColors.kPrimary, size: 28)),
         ],
       ),
     );
