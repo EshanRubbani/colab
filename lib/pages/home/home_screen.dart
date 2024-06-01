9435544ecfb71e1e1db6a676e8429ba5c4bc4a14
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-06-01 05:37:16 +0500

    home screen updated for mobile

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -170,6 +170,14 @@ Widget _buildForMobile(Size size) {
                             post['ownerName'],
                             style: const TextStyle(fontSize: 16),
                           ),
+                          const Text(
+                                 "  ",
+                                  style: TextStyle(fontSize: 22),
+                                ),
+                                Text(
+                                  post['selectedItemType'],
+                                  style: const TextStyle(fontSize: 10,color: Colors.grey  ),
+                                ),
                         ],
                       ),
                       const SizedBox(height: 15),
