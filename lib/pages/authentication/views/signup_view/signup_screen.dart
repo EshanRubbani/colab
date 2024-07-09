d9f00bbdb05e987ed380cce72bcd16b403932d49
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:25:09 +0500

    Update signup_screen.dart

--- lib/pages/authentication/views/signup_view/signup_screen.dart
+++ lib/pages/authentication/views/signup_view/signup_screen.dart
@@ -1,10 +1,10 @@
-import 'package:collab/extras/common/common_button.dart';
-import 'package:collab/extras/utils/res.dart';
-import 'package:collab/pages/authentication/views/login_view/login_screen.dart';
-import 'package:collab/pages/authentication/views/phone_signup/phone_signup.dart';
-import 'package:collab/pages/authentication/views/profile_image/profile_imageD.dart';
-import 'package:collab/extras/utils/constant/colors.dart';
-import 'package:collab/pages/authentication/views/profile_image/profile_imageM.dart';
+import 'package:Collab/extras/common/common_button.dart';
+import 'package:Collab/extras/utils/res.dart';
+import 'package:Collab/pages/authentication/views/login_view/login_screen.dart';
+import 'package:Collab/pages/authentication/views/phone_signup/phone_signup.dart';
+import 'package:Collab/pages/authentication/views/profile_image/profile_imageD.dart';
+import 'package:Collab/extras/utils/constant/colors.dart';
+import 'package:Collab/pages/authentication/views/profile_image/profile_imageM.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -20,71 +20,90 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   final emailController = TextEditingController();
-
+  final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   final firstNameController = TextEditingController();
   final lastNameController = TextEditingController();
 
-  final confirmPasswordController = TextEditingController();
+  
   bool isObscure = true;
   bool isObs = true;
   // Sign Up Button Logic
+void signUserUp() async {
+  // Show loading circle
+  showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      });
+  
+  try {
+    // Check if username is already taken
+    bool isUsernameTaken = await checkIfUsernameTaken(usernameController.text);
+    if (isUsernameTaken) {
+      // Pop the loading circle
+      navigator!.pop(context);
+     Get.snackbar("Invalid UserName", "User Already Taken");
+      return;
+    }
 
-  void signUserUp() async {
-    // show loading circle
-    showDialog(
-        context: context,
-        builder: (context) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        });
-    try {
-      // check if both password and confirm pasword is same
-      // if (passwordController.text == confirmPasswordController.text) {
-      UserCredential? userCredential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    // Check if both password and confirm password are the same
+
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
       );
-      // create user document and send it to firestore
-      createUserDocument(userCredential);
-      // successful event
+
+      // Create user document and send it to Firestore
+      await createUserDocument(userCredential);
+
+      // Successful event
       navigator!.pop(context);
       success();
-      // } else {
-      //   Navigator.pop(context);
-      //   genericErrorMessage("Password Doesn't Match!");
-      // }
-    } on FirebaseAuthException catch (e) {
-      //pop the loading circle
-      navigator!.pop(context);
-      genericErrorMessage(e.code);
-    }
+   
+  } on FirebaseAuthException catch (e) {
+    // Pop the loading circle
+    navigator!.pop(context);
+    genericErrorMessage(e.code);
   }
+}
 
-  //create document name with email and save additional data
-  Future<void> createUserDocument(UserCredential userCredential) async {
-    if (userCredential.user != null) {
-      try {
-        print("inside user details looop");
-        await FirebaseFirestore.instance
-            .collection("Users")
-            .doc(userCredential.user!.email.toString())
-            .set({
-          'email': userCredential.user!.email.toString(),
-          'firstName': firstNameController.text,
-          'lastName': lastNameController.text,
-          'timestamp': Timestamp.now(),
-          'userIMG': "",
-          'userUID': userCredential.user!.uid,
-        });
-        print("Firestore user details doone");
-      } catch (e) {
-        genericErrorMessage(e.toString());
-      }
+// Create document name with email and save additional data
+Future<void> createUserDocument(UserCredential userCredential) async {
+  if (userCredential.user != null) {
+    try {
+      print("Inside user details loop");
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(userCredential.user!.email.toString())
+          .set({
+        'userName': usernameController.text,
+        'email': userCredential.user!.email.toString(),
+        'firstName': firstNameController.text,
+        'lastName': lastNameController.text,
+        'timestamp': Timestamp.now(),
+        'userIMG': "",
+        'userUID': userCredential.user!.uid,
+      });
+      print("Firestore user details done");
+    } catch (e) {
+      genericErrorMessage(e.toString());
     }
   }
+}
+
+// Check if username is already taken
+Future<bool> checkIfUsernameTaken(String username) async {
+  final querySnapshot = await FirebaseFirestore.instance
+      .collection("Users")
+      .where("userName", isEqualTo: username)
+      .get();
+
+  return querySnapshot.docs.isNotEmpty;
+}
+
 
   void success() {
     showDialog(
@@ -229,6 +248,28 @@ class _SignupScreenState extends State<SignupScreen> {
                   height: size.height * 0.02,
                 ),
                 const FieldText(
+                  text: 'Username',
+                ),
+                SizedBox(
+                  height: size.height * 0.004,
+                ),
+                TextFormField(
+                  controller: usernameController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  onSaved: (username) {},
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+                const FieldText(
                   text: 'Email',
                 ),
                 SizedBox(
@@ -281,38 +322,7 @@ class _SignupScreenState extends State<SignupScreen> {
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: size.height * 0.02,
-                ),
-                const FieldText(
-                  text: 'Confirm Password',
-                ),
-                SizedBox(
-                  height: size.height * 0.004,
-                ),
-                TextFormField(
-                  obscureText: isObs,
-                  decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        isObs
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: Colors.grey[500],
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          isObs = !isObs;
-                        });
-                      },
-                    ),
-                    hintStyle: TextStyle(color: Colors.grey[400]),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
+                
                 SizedBox(
                   height: size.height * 0.03,
                 ),
