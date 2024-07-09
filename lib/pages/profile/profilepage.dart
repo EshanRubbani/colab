84dabee633a3cabe990322d221c1871ffda3ddf4
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:28:31 +0500

    Update profilepage.dart

--- lib/pages/profile/profilepage.dart
+++ lib/pages/profile/profilepage.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:collab/extras/utils/Helper/firestore.dart';
-import 'package:collab/extras/utils/res.dart';
+import 'package:Collab/extras/utils/Helper/firestore.dart';
+import 'package:Collab/extras/utils/res.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -49,146 +49,143 @@ class _ProfilePageState extends State<ProfilePage> {
             desktop: _buildForDesktop(context)));
   }
 
-  Widget _buildForDesktop(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        // Calculate the available width for the profile content
-        double availableWidth = constraints.maxWidth * 0.75; // Adjust the percentage as needed
-        double contentWidth = availableWidth < 400 ? availableWidth : 400; // Limit the maximum width
+Widget _buildForDesktop(BuildContext context) {
+  final Size size = MediaQuery.of(context).size;
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      double availableWidth = constraints.maxWidth * 0.75;
+      double contentWidth = availableWidth < 400 ? availableWidth : 400;
 
-        return Column(
-          children: [
-            SizedBox(height: 10,),
-            Container(
-              // color: Colors.red,
-              height: size.height - 160,
-              width: contentWidth, // Use calculated width
-              child: StreamBuilder<DocumentSnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('Users')
-                    .doc(userIdentifier)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final data = snapshot.data?.data() as Map<String, dynamic>?;
+      return Column(
+        children: [
+          SizedBox(height: 10),
+          Container(
+            height: size.height - 160,
+            width: contentWidth,
+            child: StreamBuilder<DocumentSnapshot>(
+              stream: userIdentifier.isNotEmpty
+                  ? FirebaseFirestore.instance
+                      .collection('Users')
+                      .doc(userIdentifier)
+                      .snapshots()
+                  : null,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.data() == null) {
+                  return const Center(child: Text('No user data found.'));
+                } else {
+                  final data = snapshot.data!.data() as Map<String, dynamic>;
 
-                    if (data == null) {
-                      return const Center(child: Text('No user data found.'));
-                    }
+                  data.remove('userUID');
+                  final userImg = data.remove('userIMG');
 
-                    return Column(
-                      children: [
-                        // Profile Picture
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage: NetworkImage(
-                            data['userIMG'] ?? 'https://via.placeholder.com/150',
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(
+                                userImg ?? 'https://via.placeholder.com/150'),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-
-                        // First Name and Last Name
-                        Text(
-                          '${data['firstName'] ?? 'First Name'} ${data['lastName'] ?? 'Last Name'}',
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+                          const SizedBox(height: 20),
+                          for (var entry in data.entries)
+                            if (entry.key != 'userUID' && entry.key != 'userIMG' && entry.key != 'joinedGroups')
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      entry.key == "timestamp"
+                                          ? "Joined on:"
+                                          : '${entry.key.capitalizeFirst} ',
+                                      style: const TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                    Spacer(),
+                                    Text(
+                                      entry.key == 'timestamp'
+                                          ? _formatTimestamp(entry.value)
+                                          : '${entry.value}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Joined Groups",
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-
-                        Text(
-                          'Desktop',
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        // Email or Phone Number
-                        Text(
-                          user.email ?? user.phoneNumber!,
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                        const SizedBox(height: 20),
-
-                        // Joined Groups
-                        const Text(
-                          "Joined Groups",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-
-                        // Display Joined Groups
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: data['joinedGroups']?.length ?? 0,
-                            itemBuilder: (context, index) {
-                              final groupId = data['joinedGroups'][index];
-                              return FutureBuilder<Map<String, dynamic>?>(
-                                future: firestoreService.fetchGroupDetails(groupId),
-                                builder: (context, groupSnapshot) {
-                                  if (groupSnapshot.connectionState == ConnectionState.waiting) {
-                                    return const ListTile(
-                                      title: Text('Loading...'),
-                                    );
-                                  } else if (groupSnapshot.hasError) {
-                                    return ListTile(
-                                      title: Text('Error: ${groupSnapshot.error}'),
-                                    );
-                                  } else if (!groupSnapshot.hasData || groupSnapshot.data == null) {
-                                    return const ListTile(
-                                      title: Text('Group not found'),
-                                    );
-                                  } else {
-                                    final groupDetails = groupSnapshot.data!;
-
-                                    return Padding(
-                                      padding: const EdgeInsets.only(left: 16.0),
-                                      child: ListTile(
-                                        title: Text(
-                                          '${index + 1}. ${groupDetails['groupName']}',
-                                          style: const TextStyle(
-                                            fontSize: 18,
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: data['joinedGroups']?.length ?? 0,
+                              itemBuilder: (context, index) {
+                                final groupId = data['joinedGroups'][index];
+                                return FutureBuilder<Map<String, dynamic>?>(
+                                  future: firestoreService.fetchGroupDetails(groupId),
+                                  builder: (context, groupSnapshot) {
+                                    if (groupSnapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return const ListTile(
+                                        title: Text('Loading...'),
+                                      );
+                                    } else if (groupSnapshot.hasError) {
+                                      return ListTile(
+                                        title: Text('Error: ${groupSnapshot.error}'),
+                                      );
+                                    } else if (!groupSnapshot.hasData ||
+                                        groupSnapshot.data == null) {
+                                      return const ListTile(
+                                        title: Text('Group not found'),
+                                      );
+                                    } else {
+                                      final groupDetails = groupSnapshot.data!;
+                                      return Padding(
+                                        padding: const EdgeInsets.only(left: 16.0),
+                                        child: ListTile(
+                                          title: Text(
+                                            '${index + 1}. ${groupDetails['groupName']}',
+                                            style: const TextStyle(fontSize: 18),
                                           ),
                                         ),
-                                      ),
-                                    );
-                                  }
-                                },
-                              );
-                            },
+                                      );
+                                    }
+                                  },
+                                );
+                              },
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                      ],
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                },
-              ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
             ),
-            SizedBox(height: 10,),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 400), // Use calculated width
-                child: BottomNavm(index: 4),
-              ),
+          ),
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 400),
+              child: BottomNavm(index: 4),
             ),
-          ],
-        );
-      },
-    );
-  }
-Widget _buildForMobile(context) {
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  
+  Widget _buildForMobile(context) {
   final size = MediaQuery.of(context).size;
   print(userIdentifier);
   return Column(
@@ -213,6 +210,10 @@ Widget _buildForMobile(context) {
             } else {
               final data = snapshot.data!.data() as Map<String, dynamic>;
 
+              // Remove userUID and userIMG from data
+              data.remove('userUID');
+              final userImg = data.remove('userIMG');
+
               return Center(
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
@@ -224,32 +225,33 @@ Widget _buildForMobile(context) {
                       CircleAvatar(
                         radius: 50,
                         backgroundImage: NetworkImage(
-                            data['userIMG'] ?? 'https://via.placeholder.com/150'),
+                            userImg ?? 'https://via.placeholder.com/150'),
                       ),
                       const SizedBox(height: 20),
 
-                      // First Name and Last Name
-                      Text(
-                        '${data['firstName'] ?? 'First Name'} ${data['lastName'] ?? 'Last Name'}',
-                        style: const TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 10),
+                      // Display all fields dynamically except userUID and userIMG
+                      for (var entry in data.entries)
+                        if (entry.key != 'userUID' && entry.key != 'userIMG'&& entry.key != 'joinedGroups')
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                 entry.key == "timestamp"? "Joined on:": '${entry.key.capitalizeFirst} ',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Spacer(),
+                                Text(
+                                  entry.key == 'timestamp'
+                                      ? _formatTimestamp(entry.value)
+                                      : '${entry.value}',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
 
-                      Text(
-                        'Mobile',
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-
-                      // Email or Phone Number
-                      Text(
-                        user.email ?? user.phoneNumber!,
-                        style: const TextStyle(fontSize: 18),
-                      ),
                       const SizedBox(height: 20),
 
                       // Joined Groups
@@ -290,6 +292,7 @@ Widget _buildForMobile(context) {
                                       '${index + 1}. ${groupDetails['groupName']}',
                                       style: const TextStyle(
                                         fontSize: 18,
+                                        fontWeight: FontWeight.bold
                                       ),
                                     ),
                                   );
@@ -315,4 +318,10 @@ Widget _buildForMobile(context) {
     ],
   );
 }
+
+String _formatTimestamp(Timestamp timestamp) {
+  final DateTime date = timestamp.toDate();
+  return '${date.day}-${date.month}-${date.year}';
+}
+
 }
