1ae8f5d829f18ee73afd702d724dca4577b34626
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:23:00 +0500

    Update forgot_password_screen.dart

--- lib/pages/authentication/views/forgot_password_view/forgot_password_screen.dart
+++ lib/pages/authentication/views/forgot_password_view/forgot_password_screen.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:collab/extras/common/common_button.dart';
-import 'package:collab/pages/authentication/views/login_view/login_screen.dart';
-import 'package:collab/pages/authentication/views/verify_account_view/signup_verification_screen.dart';
-import 'package:collab/extras/utils/constant/colors.dart';
+import 'package:Collab/extras/common/common_button.dart';
+import 'package:Collab/pages/authentication/views/login_view/login_screen.dart';
+import 'package:Collab/pages/authentication/views/verify_account_view/signup_verification_screen.dart';
+import 'package:Collab/extras/utils/constant/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
