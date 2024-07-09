887bc2f1aae26294822e44ac2e03f621cfe179a2
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:24:13 +0500

    Update onboard_elevated_button.dart

--- lib/pages/authentication/views/onboard_view/widgets/onboard_elevated_button.dart
+++ lib/pages/authentication/views/onboard_view/widgets/onboard_elevated_button.dart
@@ -1,4 +1,4 @@
-import 'package:collab/extras/utils/constant/colors.dart';
+import 'package:Collab/extras/utils/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 import '../../../controllers/controller.onboarding/onboarding_controller.dart';
