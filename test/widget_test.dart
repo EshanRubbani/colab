8eb89b09efe56e6d14b5fddd9332efba4ae47872
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:28:44 +0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:collab/main.dart';
+import 'package:Collab/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
