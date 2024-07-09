8838a528f2f90032f668d673e23223d92c88673d
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 20:22:57 +0500

    Update create_new_password_screen.dart

--- lib/pages/authentication/views/create_new_password/create_new_password_screen.dart
+++ lib/pages/authentication/views/create_new_password/create_new_password_screen.dart
@@ -1,6 +1,6 @@
-import 'package:collab/extras/common/common_button.dart';
-import 'package:collab/extras/utils/constant/colors.dart';
-import 'package:collab/extras/utils/constant/device_size.dart';
+import 'package:Collab/extras/common/common_button.dart';
+import 'package:Collab/extras/utils/constant/colors.dart';
+import 'package:Collab/extras/utils/constant/device_size.dart';
 import 'package:flutter/material.dart';
 
 class CreateNewPasswordScreen extends StatefulWidget {
