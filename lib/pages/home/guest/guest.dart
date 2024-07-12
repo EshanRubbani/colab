d8a10ac429148ef4e744472fffc38b860a1b9e16
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-12 14:33:18 +0500

    Update guest.dart

--- lib/pages/home/guest/guest.dart
+++ lib/pages/home/guest/guest.dart
@@ -1,4 +1,3 @@
-
 import 'package:Collab/pages/home/guest/mobileGuest.dart';
 import 'package:Collab/pages/authentication/views/login_or_signup_view/login_or_signup_screen.dart';
 import 'package:Collab/extras/utils/constant/colors.dart';
@@ -17,17 +16,13 @@ class Guest extends StatefulWidget {
 }
 
 class _GuestState extends State<Guest> {
-  
   void signUserOut() {
     FirebaseAuth.instance.signOut();
     Get.to(() => const LoginOrSignupScreen());
   }
 
- 
-
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
         surfaceTintColor: Colors.white,
@@ -36,13 +31,31 @@ class _GuestState extends State<Guest> {
         forceMaterialTransparency: true,
         elevation: 0,
         actions: [
-          
-         ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: KAppColors.kPrimary),onPressed: () => Get.to(LoginOrSignupScreen()), child: Row(children: [
-          Text("Log in",style: TextStyle(color: Colors.white,fontFamily: "Popins"),),
-          SizedBox(width: 10,),
-          Icon(Icons.login_rounded,color: Colors.white,size: 15,),
-         ],)),
-         SizedBox(width: 20,)
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: KAppColors.kPrimary),
+              onPressed: () => Get.to(() => const LoginOrSignupScreen(),
+                  transition: Transition.cupertinoDialog,
+                  duration: Duration(seconds: 1)),
+              child: Row(
+                children: [
+                  Text(
+                    "Log in",
+                    style: TextStyle(color: Colors.white, fontFamily: "Popins"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.login_rounded,
+                    color: Colors.white,
+                    size: 15,
+                  ),
+                ],
+              )),
+          SizedBox(
+            width: 20,
+          )
         ],
       ),
       body: const ResponsiveNess(
@@ -51,6 +64,4 @@ class _GuestState extends State<Guest> {
       ),
     );
   }
-
-
 }
